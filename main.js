e7d6da81b13be7faeb4dbdf15bec27bfe5f4deb6
Melissa Bilher <mbilherr@gmail.com>
2021-12-19 16:48:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,9 @@ const url = `https://api.github.com/users/${LinksSocialMedia.github}`
 
 fetch(url).then(response => response.json()).then(data => {userName.textContent = data.name
 userBio.textContent = data.bio
-userLink.href = data.html_url})
+userLink.href = data.html_url
+userImage.src = data.avatar_url
+userLogin.textContent = data.login})
 }
 
 getGitHubProfileInfos()
